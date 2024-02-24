7c446dadf7d30a784e8115b8374e6dc10df3cc5e
Ahsan <ahsaninfo1@gmail.com>
2024-02-24 10:44:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <h3>GitHub Profile Readme Generator</h3>
+    </div>
+  )
+}
+
+export default App
