e31b25a58b039e9dc2f07df783519c7b124d1094
Ahsan <ahsaninfo1@gmail.com>
2024-02-27 14:38:54 +0530

    first commit

---
+++ src/Data/LanguageBadges.jsx
@@ -0,0 +1,187 @@
+export const LanguageBadges = [
+    {
+        name: "Apache Groovy",
+        image: "Apache%20Groovy-4298B8.svg?logo=Apache+Groovy&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "C",
+        image: "c-%2300599C.svg?logo=c&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "C#",
+        image: "c%23-%23239120.svg?logo=c-sharp&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "C++",
+        image: "c++-%2300599C.svg?logo=c%2B%2B&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "Crystal",
+        image: "crystal-%23000000.svg?logo=crystal&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "CSS3",
+        image: "css3-%231572B6.svg?logo=css3&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "Clojure",
+        image: "Clojure-%23Clojure.svg?logo=Clojure&logoColor=Clojure",
+        isSelected: false,
+    },
+    {
+        name: "Dart",
+        image: "dart-%230175C2.svg?logo=dart&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "Elixir",
+        image: "elixir-%234B275F.svg?logo=elixir&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "Elm",
+        image: "Elm-60B5CC?logo=elm&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "Erlang",
+        image: "Erlang-white.svg?logo=erlang&logoColor=a90533",
+        isSelected: false,
+    },
+    {
+        name: "Fortran",
+        image: "Fortran-%23734F96.svg?logo=fortran&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "Go/Golang",
+        image: "go-%2300ADD8.svg?logo=go&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "GraphQL",
+        image: "-GraphQL-E10098?logo=graphql&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "Haskell",
+        image: "Haskell-5e5086?logo=haskell&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "HTML5",
+        image: "html5-%23E34F26.svg?logo=html5&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "Java",
+        image: "java-%23ED8B00.svg?logo=java&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "JavaScript",
+        image: "javascript-%23323330.svg?logo=javascript&logoColor=%23F7DF1E",
+        isSelected: false,
+    },
+    {
+        name: "Julia",
+        image: "-Julia-9558B2?logo=julia&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "Kotlin",
+        image: "kotlin-%230095D5.svg?logo=kotlin&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "LaTeX",
+        image: "latex-%23008080.svg?logo=latex&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "Lua",
+        image: "lua-%232C2D72.svg?logo=lua&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "Markdown",
+        image: "markdown-%23000000.svg?logo=markdown&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "Nim",
+        image: "nim-%23FFE953.svg?logo=nim&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "Octave",
+        image: "OCTAVE-darkblue?logo=octave&logoColor=fcd683",
+        isSelected: false,
+    },
+    {
+        name: "PHP",
+        image: "php-%23777BB4.svg?logo=php&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "Perl",
+        image: "perl-%2339457E.svg?logo=perl&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "Python",
+        image: "python-3670A0?logo=python&logoColor=ffdd54",
+        isSelected: false,
+    },
+    {
+        name: "R",
+        image: "r-%23276DC3.svg?logo=r&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "Ruby",
+        image: "ruby-%23CC342D.svg?logo=ruby&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "Rust",
+        image: "rust-%23000000.svg?logo=rust&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "Scala",
+        image: "scala-%23DC322F.svg?logo=scala&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "Shell Script",
+        image: "shell_script-%23121011.svg?logo=gnu-bash&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "Solidity",
+        image: "Solidity-%23363636.svg?logo=solidity&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "Swift",
+        image: "swift-F54A2A?logo=swift&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "TypeScript",
+        image: "typescript-%23007ACC.svg?logo=typescript&logoColor=white",
+        isSelected: false,
+    },
+    {
+        name: "Zig",
+        image: "Zig-%23F7A41D.svg?logo=zig&logoColor=white",
+        isSelected: false,
+    },
+];
